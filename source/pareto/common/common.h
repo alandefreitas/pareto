8273bc3e69a529df9f6f60b07980f8105f5a2aae
alandefreitas <alandefreitas@gmail.com>
2020-12-23 04:57:10 -0300

    Deprecate init list to vector function

--- source/pareto/common/common.h
+++ source/pareto/common/common.h
@@ -11,7 +11,7 @@
 namespace pareto {
     /// \brief Convert an initializer list to a vector
     /// This is a minor convenience for some containers
-    inline std::vector<uint8_t> init_list_to_vector(std::initializer_list<bool> is_minimization) {
+    [[maybe_unused,deprecated]] inline std::vector<uint8_t> init_list_to_vector(std::initializer_list<bool> is_minimization) {
         std::vector<uint8_t> v(is_minimization.begin(), is_minimization.end());
         return v;
     }
