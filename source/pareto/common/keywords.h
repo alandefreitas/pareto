b2cfe57f3660e499bcc4b913c7fbc16389722751
alandefreitas <alandefreitas@gmail.com>
2020-12-23 04:57:39 -0300

    Update keywords for target directions

---
+++ source/pareto/common/keywords.h
@@ -0,0 +1,15 @@
+//
+// Created by Alan Freitas on 12/12/20.
+//
+
+#ifndef PARETO_KEYWORDS_H
+#define PARETO_KEYWORDS_H
+
+namespace pareto {
+    constexpr bool minimization = true;
+    constexpr bool min = true;
+    constexpr bool maximization = false;
+    constexpr bool max = false;
+}
+
+#endif // PARETO_KEYWORDS_H
